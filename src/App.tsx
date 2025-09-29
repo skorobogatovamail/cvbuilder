5c583caf6c617c58916323aaa578bf25822e1e7a
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-09-29 10:34:19 +0300

    add dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { MainLayout } from './layouts/MainLayout';
 import { lazy, Suspense } from 'react';
+import { Dashboard } from './features/Dashboard/Dashboard';
 
 const Home = lazy(() => import('./features/Home/Home'));
 const EditResume = lazy(() => import('./features/EditResume/EditResume'));
@@ -15,6 +16,7 @@ export const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
           <Route path="edit/:resumeId" element={<EditResume />} />
           <Route path="*" element={<Navigate to={'/'} replace />} />
         </Route>
