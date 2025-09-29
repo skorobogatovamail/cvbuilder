34a1d8572c5c24e71d87e8d7833283069184b525
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-09-29 10:33:57 +0300

    fix main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,13 @@
 import { Header } from '@/components/Header/Header';
+import { Outlet } from 'react-router-dom';
 
-export const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+export const MainLayout: React.FC = () => {
   return (
     <div>
       <Header />
-      <main>{children}</main>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 };
